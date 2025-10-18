873378074f820218f755b3aac8189fcd9c14402b
sowri <sowri.srikanth@gmail.com>
2025-10-18 10:47:31 +0530

    Added simple comment

--- SpringBootCrudExample/src/main/java/com/SpringCrud1/controller/ProductController.java
+++ SpringBootCrudExample/src/main/java/com/SpringCrud1/controller/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.SpringCrud1.entity.Product;
 import com.SpringCrud1.service.ProductService;
 
+//Simple comment added
 @RestController
 public class ProductController {
 	
