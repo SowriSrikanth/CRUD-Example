6557d03764979a58938eb86c918b7e04be6e2585
sowri <sowri.srikanth@gmail.com>
2025-10-18 10:56:24 +0530

    Modified again after reversion

--- SpringBootCrudExample/src/main/java/com/SpringCrud1/controller/ProductController.java
+++ SpringBootCrudExample/src/main/java/com/SpringCrud1/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.SpringCrud1.entity.Product;
 import com.SpringCrud1.service.ProductService;
 
-//Simple comment added
+//Simple comment added-modified after reversion
 @RestController
 public class ProductController {
 	
