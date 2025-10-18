f2bc4b4ae46bcba489f3007d88e7bfac4d9e84e8
sowri <sowri.srikanth@gmail.com>
2025-10-18 10:54:29 +0530

    Revert "modified the comment"
    This reverts commit e360fd30f1fc1cc675001b2c11e1301cc654745e.

--- SpringBootCrudExample/src/main/java/com/SpringCrud1/controller/ProductController.java
+++ SpringBootCrudExample/src/main/java/com/SpringCrud1/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.SpringCrud1.entity.Product;
 import com.SpringCrud1.service.ProductService;
 
-//Simple comment added-modified
+//Simple comment added
 @RestController
 public class ProductController {
 	
